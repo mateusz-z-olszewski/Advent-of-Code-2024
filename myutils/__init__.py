8b0a3d0e123b7e64510fdfce198e4d8f27f3283f
Mateusz Olszewski <matix2k4@outlook.com>
2024-12-10 08:25:33 +0100

    Optimized imports

--- myutils/__init__.py
+++ myutils/__init__.py
@@ -1,7 +1,13 @@
+# Common libraries
+import numpy as np
+import itertools
+import functools
+
+# Local utility functions
+from template import AOC
 from parsingdataclass import parsing
 from functionutils import *
 from iterutils import *
 from misc import *
 from parallel import *
 from parsing import *
-from template import AOC
